2cfbdbae417f3303a31065a259960f3d4ea7ea98
Jose Ferreira Claro Junior <juniorsenaidev@gmail.com>
2026-04-15 21:54:41 -0300

    ATUALIZAÇÃO MAIS RECENTE

--- Models/ViewModels/ApiViewModels.cs
+++ Models/ViewModels/ApiViewModels.cs
@@ -22,7 +22,7 @@ namespace LH_PET_WEB.Models.ViewModels
 
         [Required(ErrorMessage = "O CPF é obrigatório.")]
         [Cpf(ErrorMessage = "O CPF informado é matematicamente inválido.")]
-        public string Cpf { get; set; } = string.Empty;
+        public string CPF { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "O Telefone é obrigatório.")]
         [RegularExpression(@"^\(?\d{2}\)?[\s-]?\d{4,5}-?\d{4}$", ErrorMessage = "Formato de telefone inválido.")]
@@ -57,7 +57,7 @@ namespace LH_PET_WEB.Models.ViewModels
 
         public string Tipo { get; set; } = string.Empty;
     }
-    public class ApiperfilUpdateDTO
+    public class ApiPerfilUpdateDTO
     {
         [Required]
         [MinLength(3)]
